4a7149fa76f7923cf10924a111923080d92fa3e5
kantuno <connofitz@gmail.com>
2020-10-09 21:04:13 -0500

    Added documentation

--- src/js/engine.js
+++ src/js/engine.js
@@ -5,6 +5,9 @@ const UP_KEY = 38;
 const RIGHT_KEY = 39;
 const DOWN_KEY = 40;
 
+/**
+ * Main game engine class
+ */
 class Engine{
     #canvas;
     #ctx;
@@ -12,6 +15,10 @@ class Engine{
     #objects;
     #width;
 
+    /**
+     * Constructor for the Engine class
+     * @param {HTML div element} div - The root div where the engine will create a canvas as a child. 
+     */
     constructor(div){
         let canvas = document.createElement("CANVAS");
         div.appendChild(canvas);
@@ -23,6 +30,11 @@ class Engine{
         this.#width = 100;
     }
 
+    /**
+     * Initialize the game canvas by setting its height, width, and adding the css gameWindow class to it.
+     * @param {number} width - The width in pixels of the game canvas. Defaults to 100.
+     * @param {number} height - The height in pixels of the game canvas. Defaults to 100.
+     */
     init(width, height){
         if(arguments.length === 2){
             this.#height = height;
@@ -30,23 +42,40 @@ class Engine{
         }
 
         this.#canvas.classList.add("gameWindow");
-        this.#canvas.style.width = this.#width.toString() + "px";
-        this.#canvas.style.height = this.#height.toString() + "px";
+        this.#canvas.setAttribute('width', this.#width.toString());
+        this.#canvas.setAttribute('height', this.#height.toString());
     }
 
+    /**
+     * Moves a GameObject to a position.
+     * @param {GameObject} obj - An instance of a GameObject or a class that extends it.
+     * @param {Object} newpos - The position in format {x, y} where the object will be moved to. 
+     */
     #moveToPos(obj, newpos){
         obj.pos = {x: newpos.x, y: newpos.y};
     }
 
+    /**
+     * Moves a GameObject relative to where it currently is.
+     * @param {GameObject} obj - An instance of a GameObject or a class that extends it. 
+     * @param {Object} vect - Values to move the object horizontally and/or vertically in format {x, y}.
+     */
     #translate(obj, vect){
         obj.pos = {x: obj.pos.x + vect.x, y: obj.pos.y + vect.y};
     }
 
+    /**
+     * Clears the canvas and redraws everything contained within the objects array.
+     */
     #render(){
         this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
         this.#objects.forEach(obj => obj.draw(this.#ctx));
     }
 
+    /**
+     * Handles keyboard input
+     * @param {Event} e - The event triggered by a key press.
+     */
     #keyinput(e){
         let platform = this.#getObjectByName("plat");
 
@@ -62,6 +91,12 @@ class Engine{
         }
     }
 
+    /**
+     * Checks whether a point is within a rectangle.
+     * @param {DOMRect} rect - A rectange. 
+     * @param {Object} pos - The point stored in format {x, y}.
+     * @return {Boolean} True if the point is within the rectangle, otherwise false.
+     */
     #isInBounds(rect, pos){
         if(pos.x > rect.left && pos.x < rect.left + rect.width && pos.y > rect.top && pos.y < rect.top + rect.height){
             return true;
@@ -71,11 +106,23 @@ class Engine{
         }
     }
 
+    /**
+     * Handles mouse click events.
+     * @param {Event} e - The event triggered by a mouse click. 
+     */
     #mouseinput(e){
-        let platform = this.#getObjectByName("plat");
-        this.#moveToPos(platform, {x: e.clientX, y: e.clientY});
+        let rect = this.#canvas.getBoundingClientRect();
+        if(this.#isInBounds(rect, {x: e.clientX, y: e.clientY})){
+            let platform = this.#getObjectByName("plat");
+            this.#moveToPos(platform, {x: e.clientX - rect.left, y: e.clientY - rect.top});
+        }
     }
 
+    /**
+     * Checks to see whether a GameObject with a given name exists within the objects array.
+     * @param {String} name
+     * @return {GameObject} The object from the objects array with the name given.
+     */
     #getObjectByName(name){
         let found = false;
         let returnObj;
@@ -94,6 +141,25 @@ class Engine{
         return returnObj;
     }
 
+    /**
+     * Detects collision between two given objects, or one object and everything in the objects array.
+     * @param {GameObject} objOne - The first object. 
+     * @param {GameObject} objTwo - The second object. Optional, if not given objOne is checked against every element of objects.
+     * @return {GameObject} The object objOne collided with, or null if there is no collision. 
+     */
+    #detectCollision(objOne, objTwo){
+        if(arguments.length === 1){
+
+        }
+        else{
+
+        }
+    }
+
+    /**
+     * Starts the game engine.
+     * @param {number} fps - The frames per second to run the game at. Defaults to 30. 
+     */
     start(fps){
         if(typeof(fps) === 'undefined'){
             fps = 30;
@@ -102,7 +168,7 @@ class Engine{
         document.addEventListener('keydown', (e) => this.#keyinput(e));
         this.#canvas.addEventListener('mousedown', (e) => this.#mouseinput(e));
 
-        this.#objects.push(new Platform("plat", {x: 100, y: 100}, 50, "blue"));
+        this.#objects.push(new Platform("plat", {x: 100, y: 100}, 200, "blue"));
 
         setInterval(() => {
             this.#render();
--- src/js/platform.js
+++ src/js/platform.js
@@ -1,10 +1,20 @@
 import GameObject from "./gameobject.js";
 
-const PLATFORM_HEIGHT = 5;
+const PLATFORM_HEIGHT = 10;
 
+/**
+ * Class to create a flat platform.
+ */
 class Platform extends GameObject{
     #color;
 
+    /**
+     * Constructor for the Platform.
+     * @param {string} name - The name of the platform object. 
+     * @param {Object} pos - The position of the platform in format {x, y}. 
+     * @param {number} width - The width of the platform.
+     * @param {string} color - The hexadecimal value of the color for the platform. Alternatively a color name. Optional.
+     */
     constructor(name, pos, width, color){
         super(name, pos, width, PLATFORM_HEIGHT);
 
@@ -13,6 +23,9 @@ class Platform extends GameObject{
         }
     }
 
+    /**
+     * @copydoc GameObject.draw(ctx).
+     */
     draw(ctx){
         if(!(this.#color === 'undefined')){
             ctx.fillStyle = this.#color;
